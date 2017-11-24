dd1bb138d5bb125ed1beb1bc63142cbe464096b6
Marcin Zelent <zelent.marcin@gmail.com>
2017-11-24 11:00:49 +0100

    Update HomeController.php

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
     {
         // Get cURL resource
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "http://pollutometerapi.azurewebsites.net/api/Readings/latest");
+        curl_setopt($curl, CURLOPT_URL, "https://pollutometerapi.azurewebsites.net/api/Readings/latest");
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         // Send the request & save response to $resp
