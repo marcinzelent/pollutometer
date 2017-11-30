8a6064b72e36e97dd17f7d5b4efb1b23c0844a86
marcinzelent <zelent.marcin@gmail.com>
2017-11-30 12:45:39 +0100

    Added email controller.

--- src/AppBundle/Controller/NewDataController.php
+++ src/AppBundle/Controller/NewDataController.php
@@ -37,43 +37,8 @@ class NewDataController extends Controller
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
 
-        $this->sendEmail("Marcin");
-
         return $response;
 
-    }
-
-    public function sendEmail($name)
-    {
-        $message = (new \Swift_Message('Hello Email'))
-            ->setFrom('***REMOVED***')
-            ->setTo('***REMOVED***@edu.easj.dk')
-            ->setBody(
-                $this->renderView(
-                // app/Resources/views/Emails/registration.html.twig
-                    'Emails/registration.html.twig',
-                    array('name' => $name)
-                ),
-                'text/html'
-            )
-            /*
-             * If you also want to include a plaintext version of the message
-            ->addPart(
-                $this->renderView(
-                    'Emails/registration.txt.twig',
-                    array('name' => $name)
-                ),
-                'text/plain'
-            )
-            */
-        ;
-
-        //$mailer->send($message);
-
-        // or, you can also fetch the mailer service this way
-        $this->get('mailer')->send($message);
-
-        return $this->render(...);
     }
 
 }
