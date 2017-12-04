97c2270402368b629398a9c3025008295b6d0ee0
Marcin Zelent <zelent.marcin@gmail.com>
2017-12-04 13:29:05 +0100

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,3 @@
-rm -Rf app bin src tests var web composer.json composer.lock composer.phar phpunit.xml.dist README.md web.config
+rm -Rf app bin src tests var web .gitignore composer.json composer.lock composer.phar phpunit.xml.dist README.md web.config
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install
